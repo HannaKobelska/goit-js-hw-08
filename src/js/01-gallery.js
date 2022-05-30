d3bba4f8bf59f62a75c2250ab68ebac3e93368df
HannaKobelska <anna.kobelskaya@gmail.com>
2022-05-30 19:52:59 +0300

    ff

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+    <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+         </a>
+    </div>
+    `;
+        })
+        .join('');
+}
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    } else {
+        const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
+    }
+    console.log(event.target);
+}
