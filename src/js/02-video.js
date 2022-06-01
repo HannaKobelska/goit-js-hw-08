c6d90ef21e2e597c11dff220539c8b8c6947ef65
HannaKobelska <anna.kobelskaya@gmail.com>
2022-06-01 23:40:55 +0300

    fff

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+        console.log(seconds);
+        });
+},
+    1000));
+  
+
+
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+     const message = {
+        email: refs.email.value,
+        message: refs.textarea.value,
+    }
+
+    event.currentTarget.reset();
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+
+    console.log(message);
+}
+
+function onTextareaInput(event) {
+    const message = {
+        email: refs.email.value,
+        message: refs.textarea.value,
+    }
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(message));
+}
+
+function populateTextarea() {
+    try {
+        const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+        if (savedMessage) {
+            const getMessage = savedMessage.message;
+            const getEmail = savedMessage.email;
+            refs.textarea.value = getMessage;
+            refs.email.value = getEmail;
+        }
+    }
+    catch {
+        console.log("error");
+    }
+}
